6ffae1ea63f3e785a7181388afcc03387eb0804d
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-24 09:51:20 +0300

    Блокировки и обработка ошибок
    Блокировки и обработка ошибок

---
+++ module_10_3.py
@@ -0,0 +1,68 @@
+# Блокировки и обработка ошибок
+# Цель задания:
+# Практически применить знания о механизмах блокировки потоков в Python, используя класс Lock из модуля threading.
+# Задание:
+# Реализуйте программу, которая имитирует доступ к общему ресурсу с использованием механизма блокировки потоков.
+# Класс BankAccount должен отражать банковский счет с балансом и методами для пополнения и снятия денег.
+# Необходимо использовать механизм блокировки, чтобы избежать проблемы гонок (race conditions)
+# при модификации общего ресурса.
+# Примечание:
+# Используйте класс Lock из модуля threading для блокировки доступа к общему ресурсу.
+# Ожидается создание двух потоков, один для пополнения счета, другой для снятия денег.
+# Используйте with (lock object): в начале каждого метода, чтобы использовать блокировку
+
+from threading import Thread, Lock
+import threading
+
+lock = Lock()
+
+
+class BankAccount(Thread):
+
+    def __init__(self, __balance):
+        super().__init__()
+        self.balance = __balance
+
+    def deposit(self, amount):
+        with lock:
+            self.balance += amount
+            print(f"Deposited {amount}, new balance is {self.balance}")
+
+    def withdraw(self, amount):
+        with lock:
+            self.balance -= amount
+            print(f'Withdrew {amount}, new balance is {self.balance}')
+
+
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
+
+
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+
+
+account = BankAccount(1000)
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
+
+# Вывод в консоль:
+# Deposited 100, new balance is 1100
+# Deposited 100, new balance is 1200
+# Deposited 100, new balance is 1300
+# Deposited 100, new balance is 1400
+# Deposited 100, new balance is 1500
+# Withdrew 150, new balance is 1350
+# Withdrew 150, new balance is 1200
+# Withdrew 150, new balance is 1050
+# Withdrew 150, new balance is 900
+# Withdrew 150, new balance is 750
